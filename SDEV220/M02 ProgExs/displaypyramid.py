617a2c09ec1c645cb94bdc99a317d5b62b452294
lelgar <rlelgar4@gmail.com>
2021-09-13 12:14:54 -0400

    13-SEP-21_2

--- SDEV220/M02 ProgExs/displaypyramid.py
+++ SDEV220/M02 ProgExs/displaypyramid.py
@@ -7,31 +7,44 @@ Write a program that prompts the user to enter an integer from 1 to 15 and displ
         -- Using the same input number, also dissplay an  inverted triangle. 
 '''
 
-lines = int(input("Enter a number of lines: "))
-spacing = lines * 2
-for x in range(1,lines + 1):
-        s = lines + spacing        
-        sp = str(s) + "s"
-        print(format(" ",str(lines + spacing) + "s"),end='')
-        for y in range(x,0,-1):
-                print(y,end='')
-        for y in range(2,x + 1):
-                print(y,end='')
-
-        print(format(" ",sp))
-        spacing -= 3
+import math
 
 
+#       returns the base / longest row (for rightside up pyramid). 
+#       first list returned = every digit left of the center '1'; second list contains middle '1' -> end going right
+def row(input):
+        return list(reversed(range(2, input + 1))) + list(range(1, input + 1))
 
-'''
-inputInt = eval(input("Enter an integer, 1 to 15"))
-if(inputInt <= 15):
-        for i in range(1,inputInt + 1):
-                if(i != 1):
-                        middleNum = math.ceil(inputInt / 2)
-                else:
-                        middleNum = inputInt
 
-else:
-        print("ERROR:\n Input must be an integer, 1 to 15.\n Try again.")
-'''
+#       prints rightside up pyramid. uses foreach incrementing from 1 to input to print rows starting with '1' -> '2 1 2' -> ...
+def printPyramid(rows):
+        for i in range(1,rows + 1):
+                arrRow = row(i)
+                strRow = ""
+                for n in arrRow:
+                        strRow = str(strRow + str(n)  + " ")
+                print(strRow.center(rows * 4))                  #  format/centers rows by adding to a new string w/ 4x as many chars as input
+
+
+#       prints upside down pyramid. reversed foreach from printPyramid()
+def printUpsideDown(rows):
+        for i in range(rows,0,-1):
+                arrRow = row(i)
+                strRow = ""
+                for n in arrRow:
+                        strRow = str(strRow + str(n)  + " ")
+                print(strRow.center(rows * 4))   
+        
+
+#       main function. gets input from user for number of rows. then calls functions and 
+def main():
+        inputRows = int(input("Enter the number of lines/rows to print: "))
+        print("Pyramid: \n\n")
+        printPyramid(inputRows)
+
+        print("\n\n Upside down pyramid: \n\n")
+        printUpsideDown(inputRows)
+
+
+#       call main()
+main()
